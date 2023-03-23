53d5ce39d9b4d3166ca269bac09139fd6e6dc3fe
SMARCOCCIA <seb@gmail.fr>
2023-03-23 11:06:45 +0100

    Ajout de la route listes-utilisateurs et de la route wildcard

--- EventHub_Front/src/pages/Admin/AdminRouter.jsx
+++ EventHub_Front/src/pages/Admin/AdminRouter.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ALayout from './ALayout'
 import Dashboard from './Dashboard'
+import { ListUsers } from '@admin/User'
+import { NotFound } from '@utils/NotFound'
+
+
 
 
 const AdminRouter = () => {
@@ -9,6 +13,10 @@ const AdminRouter = () => {
     <Routes>
         <Route element={<ALayout/>}>
             <Route path='dashboard' element={<Dashboard/>}/>
+            <Route path='user'>
+              	<Route path='listes-utilisateurs' element={<ListUsers/>}/>
+            </Route>
+            <Route path="*" element={<NotFound/>}/>
         </Route>
     </Routes>
   )
