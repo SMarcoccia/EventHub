b3c8a6318258ef0db603eae6932c3d3215a9429f
SMARCOCCIA <seb@gmail.fr>
2023-03-23 11:09:26 +0100

    Refactoring du template

--- EventHub_Front/src/pages/Admin/ALayout.jsx
+++ EventHub_Front/src/pages/Admin/ALayout.jsx
@@ -1,11 +1,17 @@
+import Header from '@components/admin/Header'
+import SideMenu from '@components/admin/SideMenu'
+import { Footer } from '@components/public/Footer'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const ALayout = () => {
   return (
-    <div className='ALayout'>ALayout
+    <div className='ALayout'>
         <Header />
-        <div id="admin_body"><Outlet/></div>
+        <div id="admin">
+			<SideMenu/>
+			<div id="admin_body"><Outlet/></div>
+        </div>
         <Footer />
     </div>
   )
