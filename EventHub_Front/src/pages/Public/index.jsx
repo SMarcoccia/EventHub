6734ad88d7f29de3fc91b5edb46672c0dc066c6a
SMARCOCCIA <seb@gmail.fr>
2023-03-24 17:53:25 +0100

    Création d'un dossier Auth et déplacement des fichiers login et register dans celui-ci

--- EventHub_Front/src/pages/Public/index.jsx
+++ EventHub_Front/src/pages/Public/index.jsx
@@ -1,5 +1,5 @@
 export { default as EventDetails } from './EventDetails'
 export { default as Events } from './Events'
 export { default as Home } from './Home'
-export { default as Login } from './Login'
-export { default as Register } from './Register'
+export { default as Login } from '../Auth/Login'
+export { default as Register } from '../Auth/Register'
