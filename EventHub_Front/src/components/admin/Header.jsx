1d9e506109505c570f1af491a42948cf23815c68
SMARCOCCIA <seb@gmail.fr>
2023-03-24 16:19:38 +0100

    Ajout d'attributs tailwind dans la balise header et dans la div englobante du component SideMenu

--- EventHub_Front/src/components/admin/Header.jsx
+++ EventHub_Front/src/components/admin/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import SideMenu from './SideMenu'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='flex justify-between items-center bg-gradient-to-r from-blue-300 to-blue-400'>
+    <div className='container mx-5 justify-between py-5 flex items-center'>
+        <SideMenu/>   
+    </div>
+    </header>
   )
 }
 
