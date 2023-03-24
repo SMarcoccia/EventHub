acf815aff35fe864de06d832c3dfcee140aaee3a
SMARCOCCIA <seb@gmail.fr>
2023-03-24 16:33:18 +0100

    Suppression du component SideMenu

--- EventHub_Front/src/pages/Admin/ALayout.jsx
+++ EventHub_Front/src/pages/Admin/ALayout.jsx
@@ -1,6 +1,5 @@
 import Header from '@components/admin/Header'
-import SideMenu from '@components/admin/SideMenu'
-import { Footer } from '@components/public/Footer'
+import Footer from '@components/public/Footer'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
@@ -9,7 +8,6 @@ const ALayout = () => {
     <div className='ALayout'>
         <Header />
         <div id="admin">
-			<SideMenu/>
 			<div id="admin_body"><Outlet/></div>
         </div>
         <Footer />
