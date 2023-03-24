cb4f7048d8b396140540d84aa3b7dde4949498b8
SMARCOCCIA <seb@gmail.fr>
2023-03-24 16:27:02 +0100

    Ajout des éléments de navigation

--- EventHub_Front/src/components/admin/SideMenu.jsx
+++ EventHub_Front/src/components/admin/SideMenu.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import './sideMenu.css'
+// Image Logo :
+import imgLogo from "@img/logo.png";
 
-const SideMenu = () => {
-  return (
-    <div className='flex justify-between items-center bg-gradient-to-r from-blue-300 to-blue-400'>
-        <ul>
-            <li><Link to="/">Accueil</Link></li>
-            <li>&nbsp;</li>
-            <li><Link to="/admin/dashboard">Dashboard</Link></li>
+const SideMenu = ({title="EventHub"}) => {
+    const admin=JSON.parse(localStorage.getItem("admin"))
+    const navigate=useNavigate();
+    const path = {home: '/', dashboard: '/admin/dashboard', listusers: '/admin/user/listes-utilisateurs'}
+    return (
+    <>
+        {/* LOGO */}
+        <div
+            onClick={() => navigate('/')}
+            className='flex gap-3 items-center cursor-pointer'>
+        <img 
+            src={imgLogo} 
+            alt="logo-EventHub"
+            width={40}
+            height={40} 
+        />
+        <h3 className='text-xl font-bold'>{title}</h3>
+        </div>
+        {/* LIENS */}
+        <nav>
+        <ul className='flex justify-between gap-10'>
+            <li className='font-bold'><Link to={path.home}>Accueil</Link></li>
+            <li><Link to={path.dashboard}>Dashboard</Link></li>
             <li>Utilisteurs :
                 <ul>
-                    <li><Link to="/admin/listUsers">Liste</Link></li>
+                    <li><Link to={path.listusers}>Liste</Link></li>
                 </ul>
             </li>
-        </ul>    
-    </div>
+        </ul>
+        </nav>    
+    </>
   )
 }
 
