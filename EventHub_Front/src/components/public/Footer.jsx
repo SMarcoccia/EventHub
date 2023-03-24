8c6c1756cfd71bf759989369ca03fb88ead057ce
SMARCOCCIA <seb@gmail.fr>
2023-03-24 16:31:54 +0100

    Modification de la fonction component en default

--- EventHub_Front/src/components/public/Footer.jsx
+++ EventHub_Front/src/components/public/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-export const Footer = () => {
+const Footer = () => {
     const date = new Date().getFullYear()
   return (
     <footer className='py-4 bg-gradient-to-r from-blue-300 to-blue-400'>
-         <h3
-          className='text-lg text-gray-800 font-bold text-center'>
-          EventHub - {date} - Copyright</h3>
+		<h3
+		className='text-lg text-gray-800 font-bold text-center'>
+		EventHub - {date} - Copyright</h3>
     </footer>
   )
 }
+
+export default Footer;
 
