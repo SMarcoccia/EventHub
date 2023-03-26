4056111bd6496530511c1e716add983a127854ea
SMARCOCCIA <seb@gmail.fr>
2023-03-26 15:39:00 +0200

    Remplacement du nom Dashboard par AdminHome

--- EventHub_Front/src/pages/Admin/AdminRouter.jsx
+++ EventHub_Front/src/pages/Admin/AdminRouter.jsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ALayout from './ALayout'
-import Dashboard from './AdminHome'
 import { ListUsers } from '@admin/User'
 import { NotFound } from '@utils/NotFound'
-
-
-
+import AdminHome from './AdminHome'
 
 const AdminRouter = () => {
   return (
     <Routes>
         <Route element={<ALayout/>}>
-            <Route index element={<Dashboard/>}/>
-            <Route path='home' element={<Dashboard/>}/>
+            <Route index element={<AdminHome/>}/>
+            <Route path='home' element={<AdminHome/>}/>
             <Route path='user'>
               	<Route path='listes-utilisateurs' element={<ListUsers/>}/>
             </Route>
