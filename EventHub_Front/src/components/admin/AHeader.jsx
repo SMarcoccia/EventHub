69acb9c14deed89a6e78358fc6672eb9a1cc7e39
SMARCOCCIA <seb@gmail.fr>
2023-03-27 10:03:33 +0200

    Ajout d'un id sideMenu pour supprimer l'effet de bord sur les autres pages du projet ; notamment sur la pagination

--- EventHub_Front/src/components/admin/AHeader.jsx
+++ EventHub_Front/src/components/admin/AHeader.jsx
@@ -4,7 +4,7 @@ import SideMenu from './SideMenu'
 const AHeader = () => {
   return (
     <header className='flex justify-between items-center bg-gradient-to-r from-blue-300 to-blue-400'>
-    <div className='container mx-5 justify-between py-5 flex items-center'>
+    <div id='sideMenu' className='container mx-5 justify-between py-5 flex items-center'>
         <SideMenu/>   
     </div>
     </header>
