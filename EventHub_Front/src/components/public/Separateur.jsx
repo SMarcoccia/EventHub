973b50018c2353afc5713a438536ea8b05ef4c52
SMARCOCCIA <seb@gmail.fr>
2023-03-28 17:51:45 +0200

    Ajout d'une hauteur en css tailwind

--- EventHub_Front/src/components/public/Separateur.jsx
+++ EventHub_Front/src/components/public/Separateur.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Separateur = () => {
   return (
-    <div className="h-24 w-full bg-gradient-to-r from-blue-300 to-blue-400  mb-5">
+    <div className="h-12 bg-gradient-to-r from-blue-300 to-blue-400  mb-5">
             </div>
   )
 }
