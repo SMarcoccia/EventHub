4f414d6a2f42c22bf393f3ed0e4e65a54fc49ef2
SMARCOCCIA <seb@gmail.fr>
2023-04-10 11:09:23 +0200

    Remplacement des useState email et password par le useState credentials qui contiendra email et password, renommage dans handleSubmit du paramètre event en e

--- EventHub_Front/src/pages/Auth/Login.jsx
+++ EventHub_Front/src/pages/Auth/Login.jsx
@@ -4,19 +4,30 @@ import { Link, useNavigate } from "react-router-dom";
 
 
 const Login = () => {
-    const [email, setEmail] =useState('');
-    const [password, setPassword] =useState('');
+    const [credentials, setCredentials]=useState({
+        email:'',
+        password:''
+    });
+
+    const onChange=(e)=>{
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
     const [error, setError] =useState('');
     const navigate=useNavigate();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-        if(email == '' && password == ''){
-        setError("Champs vide")
+        // TODO faire vérif email js et password.
+        if(credentials.email == '' && credentials.password == ''){
+            setError("Champs vide")
         }else{
             axios
-            .post('http://localhost:8081/api/login', {email: email, password: password,})
+            .post('http://localhost:8081/api/login', credentials)
             .then(response => {
                 if(response.data.success){
                     localStorage.setItem("user", JSON.stringify(response.data.user));
@@ -25,8 +36,7 @@ const Login = () => {
                     setError(response.data.message);
                 }
             }).catch(error=>{
-            setError(error.response.data.message);
-            console.error(error);
+                setError(error.response.data.message);
             })
         }
     }
@@ -45,11 +55,11 @@ const Login = () => {
                 <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
-                        <input value={email} onChange={(event)=> setEmail(event.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required=""/>
+                        <input value={credentials.email} onChange={onChange} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required=""/>
                     </div>
                     <div>
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
-                        <input value={password} onChange={(event)=> setPassword(event.target.value)} type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                        <input value={credentials.password} onChange={onChange} type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                     </div>
 
                     {error && <div className="font-medium text-blue-500 text-xxs">{error}</div>}
