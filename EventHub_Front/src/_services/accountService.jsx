b9d8fd7d14fbb2af2dc0fa7203d6bf6c75227032
SMARCOCCIA <seb@gmail.fr>
2023-04-10 15:12:08 +0200

    Ajout d'une fonction fléchées isLogged

--- EventHub_Front/src/_services/accountService.jsx
+++ EventHub_Front/src/_services/accountService.jsx
@@ -1,12 +1,23 @@
 
 let setUser = (user)=>{
-    return localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("user", JSON.stringify(user));
 }
 
 let getUser = ()=>{
     return JSON.parse(localStorage.getItem("user"));
+    
 }
 
+let isLogged=()=>{
+    let user=getUser();
+    return !!user;    
+}
+
+let logout = ()=>{
+    localStorage.removeItem("user")
+}
+
+
 export const accountService={
-    setUser, getUser
+    setUser, getUser, logout, isLogged
 }
