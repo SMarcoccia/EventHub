9c89e92f36fdc97d599a61b5d13e97213e8f4c8f
SMARCOCCIA <seb@gmail.fr>
2023-04-10 14:48:45 +0200

    Ajout _services et réagencement des lignes par rapport à l'arborescence du projet

--- EventHub_Front/vite.config.js
+++ EventHub_Front/vite.config.js
@@ -8,10 +8,12 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, './src/'),
+            '@assets': resolve(__dirname, "./src/_assets"),
+            '@helpers': resolve(__dirname, "./src/_helpers"),
+            '@services': resolve(__dirname, "./src/_services"),
+            '@utils': resolve(__dirname, "./src/_utils"),
             '@components': resolve(__dirname, "./src/components/"),
             '@pages': resolve(__dirname, "./src/pages/"),
-            '@utils': resolve(__dirname, "./src/_utils"),
-            '@helpers': resolve(__dirname, "./src/_helpers"),
             '@admin': resolve(__dirname, "./src/pages/admin/"),
             '@public': resolve(__dirname, "./src/pages/public"),
             '@user': resolve(__dirname, "./src/pages/user"),
