46b316d900bf5fb9b12ffd1224b1a1690e9dd39d
SMARCOCCIA <seb@gmail.fr>
2023-04-19 10:56:34 +0200

    Suppression de plusieurs attribut dans les balises et changement de la balise section par la balise main

--- EventHub_Front/src/pages/Auth/Login.jsx
+++ EventHub_Front/src/pages/Auth/Login.jsx
@@ -50,26 +50,26 @@ const Login = () => {
     }
 
     return (
-        <section className="bg-blue-50 dark:bg-blue-900">
+        <main>
             <div className="ml-7 my-10">
                 <BackButton path={"/"}/>
             </div>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <a className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <div className="flex flex-col items-center px-6 py-8 mx-auto  lg:py-0">
+            <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
             EventHub    
-            </a>
+            </div>
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 bg-gradient-to-r from-blue-300 to-blue-400 dark:border-gray-700">
-            <div className="p-6 space-y-4 md:space-y-6 sm:p-8 ">
+            <div className="mb-8 space-y-4 md:space-y-6 sm:p-8 ">
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Connectez vous
                 </h1>
                 <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                     <div>
-                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
+                        <label htmlFor="email" className="block mb-3 text-sm font-medium text-gray-900 dark:text-white">Votre email</label>
                         <input value={credentials.email} onChange={onChange} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required=""/>
                     </div>
                     <div>
-                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
+                        <label htmlFor="password" className="block mb-3 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
                         <input value={credentials.password} onChange={onChange} type="password" name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                     </div>
 
@@ -84,11 +84,8 @@ const Login = () => {
             </div>
             </div>
             </div>
-        </section>
+        </main>
     )
   }
 
   export default Login;
-
-  
-  
