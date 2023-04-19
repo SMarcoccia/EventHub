b9b923d526ceb302ecab693699300a6558e9e885
SMARCOCCIA <seb@gmail.fr>
2023-04-19 10:46:00 +0200

    Refactoring, ajout de props au lieu de la variable event

--- EventHub_Front/src/components/public/EventCard.jsx
+++ EventHub_Front/src/components/public/EventCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export const EventCard = ({event}) => {
-    let nbCc=52;
-    let titre=event.titre;
+export const EventCard = (props) => {
+
+    let nbCc=52; // Taille max pour le titre de l'event.
+    let titre=props.event.titre;
     if (titre.length > nbCc) {
         titre = titre.slice(0, nbCc)
         titre = titre+" ...";
@@ -13,7 +14,7 @@ export const EventCard = ({event}) => {
     localStorage.setItem("path", window.location.pathname);
 
     const goToEvent=()=>{
-        navigate("/events/"+event.id);
+        navigate("/events/"+props.event.id, {state: {name: props.name}});
     }
     
     return (
@@ -22,8 +23,8 @@ export const EventCard = ({event}) => {
     <img
         onClick={goToEvent}
         className="w-full h-48 object-cover"
-        src={`data:image/jpeg;base64,${event.img}`}
-        alt={event.description} />
+        src={`data:image/jpeg;base64,${props.event.img}`}
+        alt={props.event.description} />
     <div className="px-6 py-4">
         <div
             onClick={goToEvent}
@@ -32,10 +33,10 @@ export const EventCard = ({event}) => {
         </div>
     </div>
     <div className="px-6 py-0 flex items-center justify-between">
-        <p className="text-2xl">{event.date_event.slice(0, 11)}</p>
+        <p className="text-2xl">{props.event.date_event.slice(0, 11)}</p>
     </div>
     <div className="px-6 py-0 flex items-center justify-between">
-        <p className="text-2xl">{event.prix}$</p>
+        <p className="text-2xl">{props.event.prix}$</p>
     </div>
     </div>
     )
