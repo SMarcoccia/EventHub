509c40d79867516b5e1100d50a839915065d253b
SMARCOCCIA <seb@gmail.fr>
2023-04-19 11:05:14 +0200

    Refactoring : suppression de la fonctino dateConvertFr et remplacé par celle qui est dans le fichier formatDateService. Ajout d'une fonction handlePath permettant de redirigé correctement l'utilisateur sur la page précédente lorsque l'on click sur le bouton  Backbutton

--- EventHub_Front/src/pages/Public/EventDetails.jsx
+++ EventHub_Front/src/pages/Public/EventDetails.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { BackButton } from '@components/public/BackButton'
 import { Loading } from '@components/public/Loading'
 import { accountService } from '@services/accountService'
+import { formatDateService } from '@services/formatDateService'
 
 const EventDetails = () => {
     let user = accountService.getUser();
@@ -11,77 +12,77 @@ const EventDetails = () => {
     const [event, setEvent]=useState();
     const [loading, setLoading] = useState(false)
     const params = useParams()
+    const location=useLocation();
     
     const fetchEvents = async () => {
         setLoading(true)
         await axios
         .get("http://localhost:8081/api/events/"+params.slug)
         .then((res) => { 
-            res.data.date_event=dateConvertFr(res.data.date_event);
+            res.data.date_event=formatDateService.dateConvertFr(res.data.date_event);
             setEvent(res.data)
         })
         .catch((e) => console.log(e))
         .finally(() => { setLoading(false)})
     }
 
-    const dateConvertFr = (date)=>{
-        date=new Date(date);
-        date = ('0'+date.getDate()).slice(-2)+"-"+('0'+(date.getMonth()+1)).slice(-2)+"-"+date.getFullYear()+" "+('0'+date.getHours()).slice(-2)+":"+('0'+date.getMinutes()).slice(-2);
-        return date;
+    const handlePath=()=>{
+        let path='';
+        if ( location.state.name === "Home") {
+            path='/';
+        }else if( location.state.name === "Events"){
+            path='/events';
+        }else if(user !== null){
+            path= "/user/liste-evenements-utilisateur"
+        }
+        return path;
     }
-    
+
     useEffect(() => {
-     fetchEvents()
+        fetchEvents()
     }, [])
 
   return (
     <main >
-            {
-                event && !loading ?
-                    <div className='mx-7'>
-                        <div className='py-4'>
-                            <BackButton path={ user !== null ? "/user/liste-evenements-utilisateur" : window.localStorage.getItem("path")}/>
-                        </div>
-
-                        <div className="flex">
-                            <div className="w-1/2">
-                                <img src={`data:image/jpeg;base64,${event.img}`} alt={event.titre} />
-                            </div>
-                            <div className="w-1/2 pl-10 flex flex-col justify-between">
-                            <div>
-                                <h2 className="text-5xl font-bold mb-5">{event.titre}</h2>
-                                <div className="mb-3">
-                                    <p><span className="mb-3 font-bold text-xl">Lieu : </span>{event.lieu}</p>
-                                </div>
-                                <div className="mb-3">
-                                    <p><span className="mb-3 font-bold text-xl">Date : </span>{event.date_event}</p>
-                                </div>
-                                <div className="mb-3">
-                                    <p><span className="mb-3 font-bold text-xl">Resumé : </span>{event.resume}</p>
-                                </div>
-                                <div className="mb-3">
-                                    <p className="mb-3 font-bold text-xl">Description: </p>
-                                    <p>{event.description}</p>
-                                </div>
-
-                                <div className="mb-3">
-                                    <p><span className="mb-3 font-bold text-xl">Catégorie : </span>{event.type}</p>
-                                </div>
-
-                                </div>
-                                
-                         
-                                <div className='flex justify-between mt-7'>
-                                    <p className="text-6xl font-bold">{event.prix} <span className='text-pink-700'>$</span></p>
-                                </div>
-
-                        </div>
-                        </div>
-
-                    </div> :
-                    <div className='flex justify-center'>
-                        <Loading />
+        <div className='ml-7 my-10'>
+            <BackButton path={handlePath()}/>
+        </div>
+            {event && !loading ?
+                <div className="flex mx-7">
+                    <div className="w-1/2">
+                        <img src={`data:image/jpeg;base64,${event.img}`} alt={event.titre} />
                     </div>
+                    <div className="w-1/2 pl-10 flex flex-col justify-between">
+                    <div>
+                        <h2 className="text-5xl font-bold mb-5">{event.titre}</h2>
+                        <div className="mb-3">
+                            <p><span className="mb-3 font-bold text-xl">Lieu : </span>{event.lieu}</p>
+                        </div>
+                        <div className="mb-3">
+                            <p><span className="mb-3 font-bold text-xl">Date : </span>{event.date_event}</p>
+                        </div>
+                        <div className="mb-3">
+                            <p><span className="mb-3 font-bold text-xl">Resumé : </span>{event.resume}</p>
+                        </div>
+                        <div className="mb-3">
+                            <p className="mb-3 font-bold text-xl">Description: </p>
+                            <p>{event.description}</p>
+                        </div>
+
+                        <div className="mb-3">
+                            <p><span className="mb-3 font-bold text-xl">Catégorie : </span>{event.type}</p>
+                        </div>
+
+                        </div>
+                
+                        <div className='flex justify-between mt-7'>
+                            <p className="text-6xl font-bold">{event.prix} <span className='text-pink-700'>$</span></p>
+                        </div>
+                </div>
+                </div> :
+                <div className='flex justify-center'>
+                    <Loading />
+                </div>
             }
 
         </main>
