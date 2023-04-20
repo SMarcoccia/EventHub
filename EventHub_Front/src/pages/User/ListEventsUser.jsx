7b3916ded8a3178ad637cf75e9a76469dc8e1482
SMARCOCCIA <seb@gmail.fr>
2023-04-20 10:12:51 +0200

    Ajout d'une condition si aucun événement n'a pu être chargé

--- EventHub_Front/src/pages/User/ListEventsUser.jsx
+++ EventHub_Front/src/pages/User/ListEventsUser.jsx
@@ -131,7 +131,6 @@ const ListEventsUser = () => {
         </div> :
 
         /* TABLEAU */
-        eventsBis.content.length && !loading ?
         <div>
             <div className="container max-w-7xl mx-auto mt-8">
                  <div className="flex flex-col">
@@ -158,61 +157,61 @@ const ListEventsUser = () => {
                             Action</th>
                         </tr>
                     </thead>
-
+                    {eventsBis.content.length && !loading ?
                     <tbody className="bg-white">
                         {eventsBis.content.map((event)=>
                         <tr key={event.id}>
-                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <p>{event.id}</p>
-                            </td>
-                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                            <td
+                                onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p>{event.id}</p></td>
+                            <td
+                                onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                                 <div className="text-sm leading-5 text-gray-900">
                                     <img width={60} height={60} src={`data:image/jpeg;base64,${event.img}`} alt={event.titre}/>
-                                </div>
-                            </td>
-                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                <p>{event.titre.substring(0, 45)}</p>
-                            </td>
-                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                <span>{event.resume}</span>
-                            </td>
-                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                                <span>{event.date_event}</span>
-                            </td>
+                                </div></td>
+                            <td
+                                onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p>{event.titre.substring(0, 45)}</p></td>
+                            <td
+                                onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                <span>{event.resume}</span></td>
+                            <td
+                                onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                <span>{event.date_event}</span></td>
 
-                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                            <td
+                                className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
                                 <button onClick={()=>goToCreateUpdateEvent(event)} className="text-indigo-600 hover:text-indigo-900">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                 </svg>
-                                </button>
-                            </td>
+                                </button></td>
 
-                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                            <td
+                                className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
                                 <button onClick={()=>goToEvent(event)} className="text-gray-600 hover:text-gray-900">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                 </svg>
-                                </button>
-                            </td>
+                                </button></td>
 
-                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                            <td
+                                className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
                                 <button onClick={()=>deleteEvent(event)} className="text-gray-600 hover:text-gray-900">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-600 hover:text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                 </svg>
-                                </button>
-                            </td>
+                                </button></td>
                         </tr>
                         )} 
-                    </tbody>
+                    </tbody>: <tbody><td colSpan="6" className='text-center'><Loading/></td></tbody>}
                     </table>
                     </div>
                 </div>
             </div>
             <Pagination pages={eventsBis.totalPages} fetchEvents={fetchEvents} category={""} search={search} numPage={setNumPage}/>
-        </div>: <Loading/>}
+        </div>}
     </div>
   )
 }
