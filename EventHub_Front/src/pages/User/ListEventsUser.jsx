98609ab6a09689e522ac3339450a2457cdcc1144
SMARCOCCIA <seb@gmail.fr>
2023-04-20 09:10:00 +0200

    Remplacement de la requête get par la requête getRequest du fichier accountService. Suppression du then/catch remplacé par try/catch

--- EventHub_Front/src/pages/User/ListEventsUser.jsx
+++ EventHub_Front/src/pages/User/ListEventsUser.jsx
@@ -6,23 +6,16 @@ import Pagination from '@components/public/Pagination';
 import { accountService } from '@services/accountService';
 import { formatDateService } from '@services/formatDateService';
 import SearchEvents from '@components/public/SearchEvents';
+import { Loading } from '@components/public/Loading';
 
 
 // Liste tout les événements d'un utilisateur.
 const ListEventsUser = () => {
 
-    const user=accountService.getUser();
     const navigate = useNavigate();
 
-    //const URL = "http://localhost:8081/api/events/list/";
-    //const URI_GetEvents = "user/" 
-    //const URL_GetEventsPage=URL+URI_GetEvents+user.id+"?page=";
-    //const pathEvent = "events/";
+    const user=accountService.getUser();
     const pathEventEditCreate = "/user/";
-    //const category = ""
-    //const API_URL="http://localhost:8081/api/events/user/"+user.id
-    //const numPage = 0;
-
     const URL="http://localhost:8081/api/events"
     const URL_List=URL+"/list/";
     const URL_Page="?page=";
@@ -41,7 +34,6 @@ const ListEventsUser = () => {
     const [numPage, setNumPage]=useState(0);
     const [eventsBis, setEventsBis] = useState(events);
     const [loading, setLoading]=useState(false);
-    const [pageCurrent, setPageCurrent]=useState(0);
     const [category, setCategory] = useState("");
     const [search, setSearch] = useState("");
     const [errorMsg, setErrorMsg]=useState("");
@@ -55,40 +47,30 @@ const ListEventsUser = () => {
         let userId=0;
 
         if(window.location.pathname === "/user/liste-evenements-utilisateur"){
-            // Les événements de l'utilisateur.
             userId=user.id;
         }
 
         API_URL=URL_List+userId+URL_Page+numPage+URL_Type+type+URL_Search+search;
-        await axios.get(API_URL)
-        .then(res =>
-        { 
-            if(res.data.success)
-            {
-                try {
-                    // Conversion date au format fr-FR.
-                    res.data.events.content=res.data.events.content.map((event)=>{
-                        event.date_event=formatDateService.dateConvertFr(event.date_event)
-                        return event;
-                    })
-    
-                    Object.assign(events, res.data.events)   
-                    setEventsBis(events)
-                    setErrorMsg("")
-                } catch (e) {
-                    console.log(e);
-                }
+
+        const res=await accountService.getRequest(API_URL);
+        try {
+            if(res.data.success){
+                res.data.events.content=res.data.events.content.map((event)=>{
+                    event.date_event=formatDateService.dateConvertFr(event.date_event)
+                    return event;
+                })
+                Object.assign(events, res.data.events)   
+                setEventsBis(events)
+                setErrorMsg("")
             }else{
                 setEventsBis("")
                 setErrorMsg(res.data.message);
             }
-        }).catch((e) => {
-            console.log(e)
-            setErrorMsg(e.res.data.message)
-        })
-        .finally(() => {
-                setLoading(false)
-        })
+        } catch (error) {
+            setErrorMsg(error.res.data.message)
+        }finally{
+            setLoading(false);
+        }
     }
 
     // Aller à la page de création ou d'édition d'un événemnet.
@@ -134,6 +116,7 @@ const ListEventsUser = () => {
         </div>
         <h1 className="flex justify-center font-serif text-3xl font-bold underline decoration-gray-400">Votre liste d'événements</h1>
         {successDeleteEvent && <div className='mx-10 text-center my-4 py-4 px-10 bg-lime-50 text-lime-700 shadow-sm shadow-black'>{msgDeleteEvent} {messageDeleteEvent()}</div>}
+        
         {/* DESCRIPTION */}
         <div className="flex justify-between mx-10 mt-10">
             <div><SearchEvents fetchEvents={fetchEvents} numPage={numPage} errorMsg={errorMsg} totalElements={eventsBis.totalElements}/></div>
@@ -146,6 +129,9 @@ const ListEventsUser = () => {
         <div className="flex justify-center mb-4 rounded-lg bg-info-100 px-6 py-5 text-2xl text-info-800">
             {errorMsg}
         </div> :
+
+        /* TABLEAU */
+        eventsBis.content.length && !loading ?
         <div>
             <div className="container max-w-7xl mx-auto mt-8">
                  <div className="flex flex-col">
@@ -176,57 +162,57 @@ const ListEventsUser = () => {
                     <tbody className="bg-white">
                         {eventsBis.content.map((event)=>
                         <tr key={event.id}>
-                        <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <p>{event.id}</p>
-                        </td>
-                        <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <div className="text-sm leading-5 text-gray-900">
-                                <img width={60} height={60} src={`data:image/jpeg;base64,${event.img}`} alt={event.titre}/>
-                            </div>
-                        </td>
-                        <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                            <p>{event.titre.substring(0, 45)}</p>
-                        </td>
-                        <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                            <span>{event.resume}</span>
-                        </td>
-                        <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
-                            <span>{event.date_event}</span>
-                        </td>
+                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p>{event.id}</p>
+                            </td>
+                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <div className="text-sm leading-5 text-gray-900">
+                                    <img width={60} height={60} src={`data:image/jpeg;base64,${event.img}`} alt={event.titre}/>
+                                </div>
+                            </td>
+                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                <p>{event.titre.substring(0, 45)}</p>
+                            </td>
+                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                <span>{event.resume}</span>
+                            </td>
+                            <td onClick={()=>goToEvent(event)} className="cursor-pointer px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200">
+                                <span>{event.date_event}</span>
+                            </td>
 
-                        <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                            <button onClick={()=>goToCreateUpdateEvent(event)} className="text-indigo-600 hover:text-indigo-900">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                            </svg>
-                            </button>
-                        </td>
+                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                                <button onClick={()=>goToCreateUpdateEvent(event)} className="text-indigo-600 hover:text-indigo-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                                </button>
+                            </td>
 
-                        <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                            <button onClick={()=>goToEvent(event)} className="text-gray-600 hover:text-gray-900">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                            </svg>
-                            </button>
-                        </td>
+                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                                <button onClick={()=>goToEvent(event)} className="text-gray-600 hover:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                </svg>
+                                </button>
+                            </td>
 
-                        <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
-                            <button onClick={()=>deleteEvent(event)} className="text-gray-600 hover:text-gray-900">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-600 hover:text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                            </svg>
-                            </button>
-                        </td>
+                            <td className="text-sm font-medium leading-5 text-center whitespace-no-wrap border-b border-gray-200 ">
+                                <button onClick={()=>deleteEvent(event)} className="text-gray-600 hover:text-gray-900">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-red-600 hover:text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                                </button>
+                            </td>
                         </tr>
-                        )}
+                        )} 
                     </tbody>
                     </table>
                     </div>
                 </div>
             </div>
             <Pagination pages={eventsBis.totalPages} fetchEvents={fetchEvents} category={""} search={search} numPage={setNumPage}/>
-        </div>}
+        </div>: <Loading/>}
     </div>
   )
 }
