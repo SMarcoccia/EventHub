806a199728a4878a9e16e6c879d925e3cde5e007
SMARCOCCIA <seb@gmail.fr>
2023-04-22 14:32:55 +0200

    Suppression de la méthode getRequest

--- EventHub_Front/src/_services/accountService.jsx
+++ EventHub_Front/src/_services/accountService.jsx
@@ -1,28 +1,23 @@
-import Axios from "./callerService";
 
-let getRequest=(url)=>{
-    return Axios.get(url)
-}
-
-let setUser = (user)=>{
+const setUser = (user)=>{
     localStorage.setItem("user", JSON.stringify(user));
 }
 
-let getUser = ()=>{
+const getUser = ()=>{
     return JSON.parse(localStorage.getItem("user"));
     
 }
 
-let isLogged=()=>{
+const isLogged=()=>{
     let user=getUser();
     return !!user;    
 }
 
-let logout = ()=>{
+const logout = ()=>{
     localStorage.removeItem("user")
 }
 
 
 export const accountService={
-    getRequest, setUser, getUser, logout, isLogged
+    setUser, getUser, logout, isLogged
 }
