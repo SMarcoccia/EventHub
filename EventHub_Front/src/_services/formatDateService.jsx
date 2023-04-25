faae314be27897cc69aa6215a89f49e076c24259
SMARCOCCIA <seb@gmail.fr>
2023-04-25 10:53:17 +0200

    Création de la méthode convertAllDatesFr

--- EventHub_Front/src/_services/formatDateService.jsx
+++ EventHub_Front/src/_services/formatDateService.jsx
@@ -1,5 +1,6 @@
 
-// yyyy-MM-dd'T'HH:mm
+
+// yyyy-MM-ddTHH:mm:ss.sssZ to yyyy-MM-dd'T'HH:mm
 const dateFormatEngReduced=()=>{
     let date=new Date();
     return date.getFullYear()+"-"+('0'+(date.getMonth()+1)).slice(-2)+"-"+('0'+date.getDate()).slice(-2)+"T"+('0'+date.getHours()).slice(-2)+":"+('0'+date.getMinutes()).slice(-2)
@@ -12,6 +13,20 @@ const dateConvertFr = (date)=>{
     return date;
 }
 
+/**
+ * @param {Object} data : le tableau d'objet contenant les dates.
+ * @param {String} date : nom de la propriété associé à la date doit être une string.
+ * @returns {Object}le tableau d'objet.
+ * @description Convertit toutes les dates se trouvant dans un tableau d'objet.
+ */
+const convertAllDatesFr=(data, date)=>{
+    data.map((event)=>{
+        event[date]=dateConvertFr(event[date])
+        return event;
+    })
+    return data
+}
+
 export const formatDateService={
-    dateConvertFr, dateFormatEngReduced
+    convertAllDatesFr, dateConvertFr, dateFormatEngReduced
 }
