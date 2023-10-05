ad03ebaad49d9ad0fa65a3107f1466f00155411c
SMARCOCCIA <seb@gmail.fr>
2023-10-05 10:21:15 +0200

    Ajout du fichier SecurityConfig avec 3 Bean SecurityFilterChain, InMemoryUserDetailsManager, PasswordEncoder

--- EventHub_Back/src/main/java/fr/dawan/eventhub/security/SecurityConfig.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/security/SecurityConfig.java
@@ -1,13 +1,38 @@
-package security;
+package fr.dawan.eventhub.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
-    private PasswordEncoder passwordEncoder;
     
-    public SecurityConfig(PasswordEncoder passwordEncoder) {
-	super();
-	this.passwordEncoder = passwordEncoder;
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//	httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll();
+	httpSecurity.formLogin();
+	httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+	return httpSecurity.build();
     }
+    
+    @Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+	PasswordEncoder passwordEncoder=passwordEncoder();
+	return new InMemoryUserDetailsManager(
+		User.withUsername("salut").password(passwordEncoder.encode("1234")).authorities("USER").build()
+	);
+		
+    }
+    
+    @Bean
+    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }    
+    
 }
