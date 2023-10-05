11a0e2c7ce35e1ccc7cc0575ac1d5ea51c5b0cdd
SMARCOCCIA <seb@gmail.fr>
2023-10-05 10:14:14 +0200

    Refactoring User en AppUser

--- EventHub_Back/src/main/java/fr/dawan/eventhub/repositories/UserRepository.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/repositories/UserRepository.java
@@ -5,26 +5,24 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import fr.dawan.eventhub.Enum.Role;
-import fr.dawan.eventhub.entities.User;
+import fr.dawan.eventhub.security.entities.AppUser;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<AppUser, Long> {
 	
 	/**
 	 * SELECT FROM user WHERE 
 	 */
 	
 	@Query("FROM User u WHERE u.role=:role")
-	List<User> findAllByRole(Role role);
+	List<AppUser> findAllByRole(Role role);
 	
 	
 	@Query("From User u WHERE u.email= :email")
-	User findByEmail(@Param("email")String email);
+	AppUser findByEmail(@Param("email")String email);
 	
 	@Query("From User u WHERE u.pseudo= :pseudo")
-	User findByPseudo(@Param("pseudo") String pseudo);
+	AppUser findByPseudo(@Param("pseudo") String pseudo);
 
 }
