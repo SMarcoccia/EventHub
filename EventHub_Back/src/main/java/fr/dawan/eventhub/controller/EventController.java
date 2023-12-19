53263b2d9fca44481b5eb3621c1ac8138ac92a9c
SMARCOCCIA <seb@gmail.fr>
2023-12-19 17:46:40 +0100

    Ajout de plusieurs @PreAuthorize, refactoring méthodes findById et findAllEventsBy...

--- EventHub_Back/src/main/java/fr/dawan/eventhub/controller/EventController.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/controller/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,6 @@ import org.springframework.web.multipart.MultipartFile;
 import fr.dawan.eventhub.entities.Event;
 import fr.dawan.eventhub.service.EventService;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
@@ -36,6 +36,7 @@ public class EventController {
 	// Trouver un événement par son id.
 	@GetMapping(value="/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Event> findEventPerId(@PathVariable Long id) {
+	    	System.out.println("dans eventcontroller dans findeventperid");
 		Event event=eventService.findById(id);
 		if(event != null)
 		{
@@ -45,14 +46,14 @@ public class EventController {
 	}
 	
 	// Récupérer tous les événements ou ceux d'un utilisateur enregistré par type et par date descendante. 
-	@GetMapping(value={"/list/{id}"}, produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> findAllEventsByIdUserOrTypeDateDesc(
-			@PathVariable Long id,
+	@GetMapping(value={"/list/{username}"}, produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> findAllEventsByUsernameOrTypeDateDesc(
+			@PathVariable String username,
 			@RequestParam Map<String, String> map,
 			@PageableDefault(size=20, sort="date_event", direction=Sort.Direction.DESC) Pageable pageable){
 		Map<String, Object> response = new HashMap<>();
-		Page<Event> events=eventService.findAllEventsByIdUserAndTypeDateDesc(id, map, pageable);
-		System.out.println("maptostring : "+map.toString());
+		System.out.println("EventController dans findalleventsbyusernameortypedatedesc");
+		Page<Event> events=eventService.findAllEventsByUsernameAndTypeDateDesc(username, map, pageable);
 		if(events != null) {
 			response.put("success", true);
 			response.put("message", "Données récupérées avec succès");
@@ -66,11 +67,12 @@ public class EventController {
 	
 	// Supprimer un événement
 	@DeleteMapping(value="/{id}")
+//	@PreAuthorize("hasAuthority(SCOPE_USER)")
 	public ResponseEntity<?> deleteEvent(
 			@PathVariable Long id,
 			@PageableDefault(size=20, sort="date_event", direction=Sort.Direction.DESC) Pageable pageable){
 		// Récupération de l'utilisateur par l'événement id :
-		Long idUser=eventService.findById(id).getUser().getId();
+		String username=eventService.findById(id).getUser().getUsername();
 		// Les paramètres utilisé pour renvoyer les événements : 		
 		Map<String, String> map = new HashMap<>();
 		map.put("page", "0");
@@ -81,20 +83,22 @@ public class EventController {
 		try {
 			eventService.deleteEvent(id);
 			response.put("success", true);
-			response.put("message", "L'événement à été supprimé avec succès");
-			response.put("events", eventService.findAllEventsByIdUserAndTypeDateDesc(idUser, map, pageable));
+			response.put("events", eventService.findAllEventsByUsernameAndTypeDateDesc(username, map, pageable));
 		} catch (Exception e) {
 			response.put("success", false);
-			response.put("message", "L'événement à déjà été supprimé ou n'existe pas");
 		}
 		return ResponseEntity.ok(response);
 	}
 	
 	// Création, édition d'un événemnet.
 	@PostMapping(consumes ={MediaType.MULTIPART_FORM_DATA_VALUE}, produces={MediaType.APPLICATION_JSON_VALUE})
+//	@PreAuthorize("hasAuthority(SCOPE_USER)")
 	public ResponseEntity<Event> createUpdateEvent(
 			@RequestPart(value="file", required=false) MultipartFile file,
 			@RequestPart("event") String event) throws IOException{
+	    	System.out.println("EventController dans createUpdateEvent >>>>>>>>>>>>>>>");
+	    	System.out.println(event);
+	    	System.out.println("EventController dans createUpdateEvent <<<<<<<<<<<<<<<");
 		return ResponseEntity.ok(eventService.createUpdateEvent(event, file));
 	}
 }
