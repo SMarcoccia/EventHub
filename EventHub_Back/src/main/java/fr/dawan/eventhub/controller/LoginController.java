f6346de2d03ae7a8e6affb4dbdb4a4aa4b642e7c
SMARCOCCIA <seb@gmail.fr>
2023-12-19 17:55:12 +0100

    Ajout constructeur Lombok, refactoring nom de la variable de type userService, Décalage de ligne pour les méthodes.

--- EventHub_Back/src/main/java/fr/dawan/eventhub/controller/LoginController.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/controller/LoginController.java
@@ -12,84 +12,82 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.dawan.eventhub.Enum.Role;
 import fr.dawan.eventhub.dtos.LoginDTO;
 import fr.dawan.eventhub.security.entities.AppUser;
-import fr.dawan.eventhub.service.UserService;
+import lombok.AllArgsConstructor;
 
 
-@CrossOrigin
-@RestController
-@RequestMapping("/api")
+//@RestController
+//@RequestMapping("/api")
+@AllArgsConstructor
 public class LoginController {
 	
-	@Autowired
-	private UserService userService;
+//	private UserService userDetailsService;
 	
 	
-	@PostMapping("/register")
-	public ResponseEntity<?> register(@RequestBody AppUser user){
-		
-		AppUser u = userService.findByEmail(user.getEmail());
-		Map<String, Object> response = new HashMap<>();
-		
-		if(u!=null) {
-			
-			if(userService.findByPseudo(user.getPseudo())!=null){
-				response.put("success", false);
-				response.put("message", "L'email et le pseudo sont deja utiliser !");
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-			}		
-			response.put("success", false);
-			response.put("message", "L'email est deja utiliser !");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-		}
-		
-		if(userService.findByPseudo(user.getPseudo())!=null) {
-			response.put("success", false);
-			response.put("message", "Pseudo deja utiliser !");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-			
-		}
-		
-		user.setRole(Role.USER);
-		
-		userService.createUser(user);
-		
-		response.put("success", true);
-		response.put("message", "Utilisateur enregistrer !");
-		
-		return ResponseEntity.ok(response);
-	}
+//	@PostMapping("/register")
+//	public ResponseEntity<?> register(@RequestBody AppUser user){
+//		
+//		AppUser u = userDetailsService.findByEmail(user.getEmail());
+//		Map<String, Object> response = new HashMap<>();
+//		
+//		if(u!=null) {
+//			
+//			if(userDetailsService.findByPseudo(user.getPseudo())!=null){
+//				response.put("success", false);
+//				response.put("message", "L'email et le pseudo sont deja utiliser !");
+//				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//			}		
+//			response.put("success", false);
+//			response.put("message", "L'email est deja utiliser !");
+//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//		}
+//		
+//		if(userDetailsService.findByPseudo(user.getPseudo())!=null) {
+//			response.put("success", false);
+//			response.put("message", "Pseudo deja utiliser !");
+//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//			
+//		}
+//		
+////		user.setRole(Role.USER);
+//		
+//		userDetailsService.createUser(user);
+//		
+//		response.put("success", true);
+//		response.put("message", "Utilisateur enregistrer !");
+//		
+//		return ResponseEntity.ok(response);
+//	}
 	
 	
-	@PostMapping("/login")
-	public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO){
-
-		AppUser user = userService.findByEmail(loginDTO.getEmail());
-		Map<String, Object> response = new HashMap<>();
-		
-		if(user == null) {
-			response.put("success", false);
-			response.put("message", "Email incorrect.");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-		}
-		
-		if(!user.getPassword().equals(loginDTO.getPassword())) {
-			response.put("success", false);
-			response.put("message", "Mot de passe incorrect.");
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
-		}
-		
-		response.put("success", true);
-		response.put("message", "Authentification reussite !");		
-		response.put("user",user);
-		
-		
-		
-		return ResponseEntity.ok(response);
-		
-	}
+//	@PostMapping("/login")
+//	public ResponseEntity<?> login(@RequestBody LoginDTO loginDTO){
+//	    	System.out.println("je suis dans postmapping login");
+//		AppUser user = userService.findByEmail(loginDTO.getUsername());
+//		Map<String, Object> response = new HashMap<>();
+//		
+//		if(user == null) {
+//			response.put("success", false);
+//			response.put("message", "Email incorrect.");
+//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//		}
+//		
+//		if(!user.getPassword().equals(loginDTO.getPassword())) {
+//			response.put("success", false);
+//			response.put("message", "Mot de passe incorrect.");
+//			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+//		}
+//		
+//		response.put("success", true);
+//		response.put("message", "Authentification reussite !");		
+//		response.put("user",user);
+//		
+//		
+//		
+//		return ResponseEntity.ok(response);
+//		
+//	}
 	
 	
 	
