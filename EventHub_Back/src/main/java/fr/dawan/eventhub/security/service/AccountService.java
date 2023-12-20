5663562508201bda3d22fbc4fc10c271853be101
SMARCOCCIA <seb@gmail.fr>
2023-12-20 10:30:23 +0100

    Dans addNewUser suppression du paramètre confirmPassword, ajout des méthodes : addNewUser avec paramétre RegisterDTO, findUsrGyId, findUserByPseudo, findUserByEmail, findAllUsers, et du CRUD.

--- EventHub_Back/src/main/java/fr/dawan/eventhub/security/service/AccountService.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/security/service/AccountService.java
@@ -1,14 +1,24 @@
 package fr.dawan.eventhub.security.service;
 
+import java.util.List;
+
+import fr.dawan.eventhub.dtos.RegisterDTO;
 import fr.dawan.eventhub.security.entities.AppRole;
 import fr.dawan.eventhub.security.entities.AppUser;
 
 public interface AccountService {
 
-   AppUser addNewUser(String username, String lastname, String password, String email, String confirmPassword, String pseudo);
+   AppUser addNewUser(String username, String lastname, String password, String email, String pseudo);
+   AppUser addNewUser(RegisterDTO registerDTO);
    AppRole addNewRole(String role);
    void addRoleToUser(String username, String role);
    void removeRoleFromUser(String username, String role);
+//   AppUser findUserById(Long id);
    AppUser loadUserByUsername(String username);
-    
+   AppUser findUserByPseudo(String pseudo);
+   AppUser findUserByEmail(String email);
+   List<AppUser> findAllUsers();
+   void deleteUser(Long id);
+   AppUser createUser(AppUser user);
+   AppUser updateUser(AppUser user);
 }
