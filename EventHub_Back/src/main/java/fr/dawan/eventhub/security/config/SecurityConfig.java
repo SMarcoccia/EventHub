e230dd9c6cd845d71723730e39ca219be3e2efeb
SMARCOCCIA <seb@gmail.fr>
2023-12-20 10:18:37 +0100

    Ajout des attributs : RSAKeys, passwordEncoder et userDetailsServiceImpl et ajout des Beans : jwtDecoder, jwtEncoder, authenticationManager et corsConfigurationSource. Ajout dans la méthode securityFilterChain de divers méthodes.

--- EventHub_Back/src/main/java/fr/dawan/eventhub/security/config/SecurityConfig.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/security/config/SecurityConfig.java
@@ -1,38 +1,113 @@
-package fr.dawan.eventhub.security;
+package fr.dawan.eventhub.security.config;
 
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import fr.dawan.eventhub.security.service.impl.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled=true)
+@AllArgsConstructor
 public class SecurityConfig {
     
-    
+    private RSAKeysConfig rsaKeysConfig;
+    private PasswordEncoder passwordEncoder;
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//	httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/").permitAll();
-	httpSecurity.formLogin();
-	httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
-	return httpSecurity.build();
+	return httpSecurity
+                // Authentification par un formulaire loginPage pour un formulaire personnalisé si on travail avec une architecture
+                // monolitique (mvc).
+                // httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/", true).permitAll();
+                // On gère la session côté front (on utilise un jwt).
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Quand on utilise Stateless on doit désactiver la protection csrf;
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth->auth.requestMatchers(
+                	"/api/events/**",
+                	"/auth/login/**", 
+                	"/auth/register/**", 
+                	"/auth/profile", 
+                	"/swagger-ui/**", 
+                	"/v3/api-docs/**", 
+                	"/swagger-ui.html", 
+                	"/war/**, /jar/**").permitAll())
+                .authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .exceptionHandling(eh -> eh.accessDeniedPage("/notAUrhorized"))
+                // .exceptionHandling().accessDeniedPage("/notAUrhorized") // Not working.
+                // Pour pouvoir faire une authentication avec Jwt.
+                // oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt); ou :
+                .oauth2ResourceServer(oa -> oa.jwt(Customizer.withDefaults()))
+                //httpSecurity.rememberMe();
+//		.authenticationManager()
+                .userDetailsService(userDetailsServiceImpl)
+                .build();
     }
-    
+
     @Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-	PasswordEncoder passwordEncoder=passwordEncoder();
-	return new InMemoryUserDetailsManager(
-		User.withUsername("salut").password(passwordEncoder.encode("1234")).authorities("USER").build()
-	);
-		
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(rsaKeysConfig.publicKey()).build();
     }
-    
+
     @Bean
-    PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }    
-    
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeysConfig.publicKey()).privateKey(rsaKeysConfig.privateKey()).build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.isHideUserNotFoundExceptions();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
 }
+
