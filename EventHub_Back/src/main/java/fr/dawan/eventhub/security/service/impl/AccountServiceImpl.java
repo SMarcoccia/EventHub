826df73f9ab6877c035d75c398513a1cbf899f7c
SMARCOCCIA <seb@gmail.fr>
2023-12-20 10:40:02 +0100

    Implémentation des méthodes : AddNewUser avec paramètre registerDTO, AddNewUser avec paramètre de la classe AppUser, addRoleToUser, findUserByEmail, findUserByPseudo, findAllUsers, et CRUD.

--- EventHub_Back/src/main/java/fr/dawan/eventhub/security/service/impl/AccountServiceImpl.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/security/service/impl/AccountServiceImpl.java
@@ -1,9 +1,18 @@
 package fr.dawan.eventhub.security.service.impl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.dawan.eventhub.dtos.RegisterDTO;
 import fr.dawan.eventhub.security.entities.AppRole;
 import fr.dawan.eventhub.security.entities.AppUser;
 import fr.dawan.eventhub.security.repository.AppRoleRepository;
@@ -21,13 +30,25 @@ public class AccountServiceImpl implements AccountService{
     private PasswordEncoder passwordEncoder;
     
     @Override
-    public AppUser addNewUser(String username, String lastname, String password, String email, String confirmPassword, String pseudo) {
-	AppUser appUser=appUserRepository.findByUnername(username);
+    public AppUser addNewUser(RegisterDTO registerDTO) {
+	AppUser appUser=AppUser.builder()
+		.username(registerDTO.getUsername())
+		.firstname(registerDTO.getFirstname())
+		.password(passwordEncoder.encode(registerDTO.getPassword()))
+		.email(registerDTO.getEmail())
+		.pseudo(registerDTO.getPseudo())
+		.build();
+	AppUser savedAppUser=appUserRepository.save(appUser);
+	return savedAppUser;
+    }
+    
+    @Override
+    public AppUser addNewUser(String username, String firstname, String password, String email, String pseudo) {
+	AppUser appUser=appUserRepository.findByUsername(username);
 	if(appUser!=null) throw new RuntimeException("Cette utilisateur existe déjà");
-	if(!password.equals(confirmPassword)) throw new RuntimeException("Passwords not match");
 	appUser=AppUser.builder()
 		.username(username)
-		.lastname(lastname)
+		.firstname(firstname)
 		.password(passwordEncoder.encode(password))
 		.email(email)
 		.pseudo(pseudo)
@@ -48,22 +69,67 @@ public class AccountServiceImpl implements AccountService{
 
     @Override
     public void addRoleToUser(String username, String role) {
-	AppUser appUser=appUserRepository.findByUnername(username);
+	List<AppRole> ListAppRole=new ArrayList<AppRole>();
+	
+	AppUser appUser=appUserRepository.findByUsername(username);
 	AppRole appRole=appRoleRepository.findById(role).get();
-	appUser.getRoles().add(appRole);
+	if(appUser.getRoles() == null)
+	{
+	    ListAppRole.add(appRole);
+	    appUser.setRoles(ListAppRole);
+	}else {
+	appUser.getRoles().add(appRole); 
+	    
+	}
     }
 
     @Override
     public void removeRoleFromUser(String username, String role) {
-	AppUser appUser=appUserRepository.findByUnername(username);
+	AppUser appUser=appUserRepository.findByUsername(username);
 	AppRole appRole=appRoleRepository.findById(role).get();
 	appUser.getRoles().remove(appRole);
     }
-
+    
     @Override
     public AppUser loadUserByUsername(String username) {
-	return appUserRepository.findByUnername(username);
+	return appUserRepository.findByUsername(username);
     }
 
+    @Override
+    public AppUser findUserByEmail(String email) {
+        return appUserRepository.findByEmail(email);
+    }
 
+    @Override
+    public AppUser findUserByPseudo(String pseudo) {
+        return appUserRepository.findByPseudo(pseudo);
+    }
+    
+    
+//    @Override
+//    public AppUser findUserById(Long id) {
+//	AppUser user=appUserRepository.findById(id).get();
+//	
+//        return null; 
+//    }
+
+    @Override
+    public List<AppUser> findAllUsers() {
+        return appUserRepository.findAll();
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        appUserRepository.deleteById(id);
+    }
+
+    @Override
+    public AppUser createUser(AppUser user) {
+        return appUserRepository.save(user);
+    }
+
+    @Override
+    public AppUser updateUser(AppUser user) {
+        return appUserRepository.save(user);
+    }
 }
