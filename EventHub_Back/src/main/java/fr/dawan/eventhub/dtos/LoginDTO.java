b6c1f438a979c4844eeaebe5797a57bff5c6d162
SMARCOCCIA <seb@gmail.fr>
2023-12-20 09:59:51 +0100

    Ajout lombok constructeur getter/setter et attribut username, suppression attribut email

--- EventHub_Back/src/main/java/fr/dawan/eventhub/dtos/LoginDTO.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/dtos/LoginDTO.java
@@ -1,32 +1,14 @@
 package fr.dawan.eventhub.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class LoginDTO {
 	
-	private String email;
+	private String username;
 	private String password;
-	
-	public LoginDTO(String email, String password) {
-	    super();
-	    this.email = email;
-	    this.password = password;
-	}
-	public LoginDTO() {
-	    super();
-	}
-	
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-
 }
