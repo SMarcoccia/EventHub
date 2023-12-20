45d1f6ed69d8eae285fc0a43459b7d93a6c76e63
SMARCOCCIA <seb@gmail.fr>
2023-12-20 10:26:26 +0100

    Ajout des méthodes : findByUserId, findByUsername, findByEmail, findByPseudo, findByFirstname

--- EventHub_Back/src/main/java/fr/dawan/eventhub/security/repository/AppUserRepository.java
+++ EventHub_Back/src/main/java/fr/dawan/eventhub/security/repository/AppUserRepository.java
@@ -1,9 +1,22 @@
 package fr.dawan.eventhub.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import fr.dawan.eventhub.security.entities.AppUser;
 
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findByUnername(String username);
+    
+    
+//    AppUser findByUserId(Long id);
+    AppUser findByUsername(String username);
+    	
+    AppUser findByEmail(String email);
+    
+    AppUser findByPseudo(String pseudo);
+
+    AppUser findByFirstname(String firstname);
+
 }
