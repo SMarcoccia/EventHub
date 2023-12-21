450bf32ed86b2a076aaf71f351137623da80539a
SMARCOCCIA <seb@gmail.fr>
2023-12-21 11:22:44 +0100

    Saut de ligne

--- EventHub_Front/src/components/public/SearchEvents.jsx
+++ EventHub_Front/src/components/public/SearchEvents.jsx
@@ -50,7 +50,7 @@ function SearchEvents(props) {
             }   
         </div>
             {/* RESULTAT */}
-            <p className="font-serif mt-5 text-1xl decoration-gray-400">
+            <p className="searchEventRes font-serif mt-5 text-1xl decoration-gray-400">
                 Résultat : <span className='mr-10'>
                     {props.errorMsg ? zeroEvent : props.totalElements}</span>Page {props.errorMsg ? zeroEvent : props.numPage+1}</p>
         </div>
