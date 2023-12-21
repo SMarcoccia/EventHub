55c3fc14a10b7a71c2623f14d0d49bd3d7cb8b04
SMARCOCCIA <seb@gmail.fr>
2023-12-21 16:25:13 +0100

    Saut de ligne

--- EventHub_Front/src/pages/User/UserRouter.jsx
+++ EventHub_Front/src/pages/User/UserRouter.jsx
@@ -5,11 +5,12 @@ import { NotFound } from '@utils/NotFound'
 
 
 const UserRouter = () => {
+
   return (
     <Routes>
         <Route element={<ULayout />}>
             <Route index element={<UserHome/>}/>
-            <Route path='home' element={<UserHome/>} />
+            <Route path='home' element={<UserHome/>}/>
             <Route path='liste-evenements-utilisateur' element={<ListEventsUser/>} />
             <Route path='editer-un-evenement/:id' element={<EditCreateEvent/>} />
             <Route path='creer-un-evenement' element={<EditCreateEvent/>} />
