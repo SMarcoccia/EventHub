a733ae4602465f4cefa0a8c53254297ef035cf9f
SMARCOCCIA <seb@gmail.fr>
2023-12-21 16:19:20 +0100

    Ajout des roles pour rediriger l'utilisateur vers le bon Header

--- EventHub_Front/src/pages/Public/PLayout.jsx
+++ EventHub_Front/src/pages/Public/PLayout.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 import Footer from '@components/public/Footer'
-import Header from '@components/public/Header'
+import { accountService } from '@services/accountService'
+import AHeader from '@components/admin/AHeader'
+import PHeader from '@components/public/PHeader'
+import UHeader from '@components/user/UHeader'
+
+import '../../App.css'
 
 const PLayout = () => {
+
+    let user=accountService.getUser();
+    let roles=user?.roles.includes("ADMIN");
+
     return (
-        <>
-        <Header /> 
+    <>
+        {user==null ? <PHeader /> : roles ? <AHeader/> : <UHeader/>}
         <div className='container' id="public_body"><Outlet/></div>
         <Footer />
-        </>
+    </>
     )
 }
 
