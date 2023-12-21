e07dc0715bc77aeac34662bb710036711ddf9c3f
SMARCOCCIA <seb@gmail.fr>
2023-12-21 10:24:00 +0100

    Suppression import React ajout d'une ligne vide

--- EventHub_Front/src/_helpers/AuthGuard.jsx
+++ EventHub_Front/src/_helpers/AuthGuard.jsx
@@ -1,22 +1,18 @@
 import { accountService } from '@services/accountService'
 import { Navigate } from 'react-router-dom'
-import React from 'react'
 
 const AuthGuard = ({children}) => {
+
     const user = accountService.getUser();
-    
     if( ! accountService.isLogged()){
         return <Navigate to="/"/>
     }else{
-        if (children.type.name === "AdminRouter" && user.role === "USER") {
+        if (children.type.name === "AdminRouter" && user.roles === "USER") {
             return <Navigate to="/"/>
         }else{
             return children;
         }
     }
-
-        
-    
 }
 
 export default AuthGuard
