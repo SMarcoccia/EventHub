b31a6358af2c4a07885ef3dcae5c76c72fc5b6cd
SMARCOCCIA <seb@gmail.fr>
2023-12-21 16:23:07 +0100

    Ajout des rôles pour rediriger correctement l'utilisateur

--- EventHub_Front/src/pages/User/ULayout.jsx
+++ EventHub_Front/src/pages/User/ULayout.jsx
@@ -1,13 +1,24 @@
-import Header from '@components/public/Header';
-import Footer from '@components/public/Footer';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Outlet } from 'react-router-dom';
 
+import Footer from '@components/public/Footer';
+import UHeader from '@components/user/UHeader';
+import AHeader from '@components/admin/AHeader';
+import PHeader from '@components/public/PHeader';
+import { accountService } from '@services/accountService';
+
 const ULayout = () => {
+
+    let user=accountService.getUser();
+    let roles=user?.roles.includes("ADMIN");;
+
+    useEffect(()=>{
+    }, [user])
+
     return(
     <>
-        <Header />
-        <div className='container min-h-screen' id='user_body'><Outlet/></div>
+        {user==null ? <PHeader /> : roles ? <AHeader/> : <UHeader/>}
+        <div className='container' id='user_body'><Outlet/></div>
         <Footer />
     </>
   )
