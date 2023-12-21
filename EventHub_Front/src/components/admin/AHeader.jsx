836b36ad0eacf341a302362e153fb0fa816f993d
SMARCOCCIA <seb@gmail.fr>
2023-12-21 11:11:52 +0100

    Suppression du css dans balise header et div avec l'id aSideMenu

--- EventHub_Front/src/components/admin/AHeader.jsx
+++ EventHub_Front/src/components/admin/AHeader.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import SideMenu from './SideMenu'
+import ASideMenu from './ASideMenu'
 
 const AHeader = () => {
   return (
-    <header className='flex justify-between items-center bg-gradient-to-r from-blue-300 to-blue-400'>
-    <div id='sideMenu' className='container mx-5 justify-between py-5 flex items-center'>
-        <SideMenu/>   
-    </div>
+    <header>
+    <div id='aSideMenu'><ASideMenu/></div>
     </header>
   )
 }
