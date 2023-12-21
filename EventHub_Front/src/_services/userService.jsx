8a06ac3cb9d991cd787e4a480a7d5dc05add8d3b
SMARCOCCIA <seb@gmail.fr>
2023-12-21 11:03:54 +0100

    Saut de ligne

--- EventHub_Front/src/_services/userService.jsx
+++ EventHub_Front/src/_services/userService.jsx
@@ -1,11 +1,11 @@
 import Axios from "./callerService";
 
 const register=(data)=>{
-    return Axios.post("register", data)
+    return Axios.post("auth/register", data)
 }
 
-const login=(data)=>{
-    return Axios.post("login", data)
+const login=(data, config)=>{
+    return Axios.post("auth/login", data, config)
 }
 
 export const userService={
