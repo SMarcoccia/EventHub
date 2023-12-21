c10aa2ed5cbcca8dd35c94088f6d25b268dd73e4
SMARCOCCIA <seb@gmail.fr>
2023-12-21 10:58:41 +0100

    Refactoring : tout les userId sont remplacées par (->) username et pour toutes les urls on ajoute au début api. Ajout d'une condition si data.success est null alors on convertit en date fr

--- EventHub_Front/src/_services/eventService.jsx
+++ EventHub_Front/src/_services/eventService.jsx
@@ -4,29 +4,31 @@ import { formatDateService } from "./formatDateService";
 
 
 const deleteEvent=(id)=>{
-    return Axios.delete("events/"+id)
+    return Axios.delete("api/events/"+id)
 }
 
 // Editer/créer un événement.
 const editCreateEvent=(data, config)=>{
-    return Axios.post("events", data, config);
+    return Axios.post("api/events", data, config);
 }
 
 // Récupère un événement par son id.
 const getEvent=(id)=>{
-    return Axios.get("events/"+id);
+    return Axios.get("api/events/"+id);
 }
 
-// Récupère les événements de l'utilisateur par type et/ou par recherche.
-const getAllEventsByUser= async (userId, numPage, type, search)=>{
-    const {data}=await Axios.get("events/list/"+userId+"?page="+numPage+"&type="+type+"&search="+search);
-    data.content=formatDateService.convertAllDatesFr(data.events.content, "date_event");
+// Récupérer tout les événements de l'utilisateur ou tout les événement par type et/ou par recherche.
+const getAllEventsByUser= async (username, numPage, type, search)=>{
+    const {data}=await Axios.get("api/events/list/"+username+"?page="+numPage+"&type="+type+"&search="+search);
+    if (data.success == null) {
+        data.content=formatDateService.convertAllDatesFr(data.events.content, "date_event");
+    }
     return data;
 }
 
-// Récupère les événements de l'utilisateur par type et/ou par recherche.
-const getAllEvents=(userId, numPage, type, search)=>{
-    return Axios.get("events/list/"+userId+"?page="+numPage+"&type="+type+"&search="+search);
+// Récupère tout les événements.
+const getAllEvents=(username, numPage, type, search)=>{
+    return Axios.get("api/events/list/"+username+"?page="+numPage+"&type="+type+"&search="+search);
 }
 
 export const eventService={
