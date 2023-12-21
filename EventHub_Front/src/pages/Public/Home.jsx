fe61ad4e31ea275d728820ac28be527a2277113d
SMARCOCCIA <seb@gmail.fr>
2023-12-21 16:17:47 +0100

    Ajout d'un message si session à expirée

--- EventHub_Front/src/pages/Public/Home.jsx
+++ EventHub_Front/src/pages/Public/Home.jsx
@@ -7,16 +7,37 @@ import { accountService } from "@services"
 import { eventService } from "@services"
 
 import { useQuery } from "react-query"
+import { useEffect, useState } from "react"
 
 
 const Home = () => {
 
-    let {isLoading, data} = useQuery('eventsUser', ()=>eventService.getAllEventsByUser(0, 0, "", ""));
-
+    // useQuery permet de récupérer les data et renvoi un false si les données on été chargées.
+    let {isLoading, data} = useQuery('eventsUser', ()=>eventService.getAllEventsByUser("-1", 0, "", ""));
     const user=accountService.getUser();
+    const [message, setMessage]=useState(localStorage.getItem("messageSessExpir"));
+    
+    const msgIfSessExp=()=>{
+        const time=3000;
+        setTimeout(()=>{
+            setMessage("");
+            localStorage.setItem("messageSessExpir", "")
+        }, time)
+    }
+    useEffect(()=>{
+        msgIfSessExp();
+    },[message])
 
     return (
         <main>
+        {
+            message &&  
+            <div className="flex flex-col flex-nowrap">
+                    <div className="text-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                        <span className="font-medium">{message}</span>
+                    </div>
+            </div>
+        }            
         <div className="flex justify-center px-6 py-10">
             <h1 className="text-4xl font-bold">Événements pour tous sur la région Rhône-Alpes</h1>
         </div>
@@ -53,7 +74,7 @@ const Home = () => {
                 <h3 className="text-2xl font-bold">Les événements les plus attendus</h3>
             </div>
 
-            {/* CARDEVENEMENT */}
+            {/* CARD EVENEMENT */}
             <div className="sm:flex sm:justify-center justify-around mt-10 mb-20">
             {/* Si l'on a pas d'id dans les valeurs du tableau on peut utilisé les index du tableau en 2e paramètres de map */}
             { data!==undefined  && !isLoading ? data.events.content.slice(-4).map((p) => (
