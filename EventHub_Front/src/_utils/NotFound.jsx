6f35ad2a7692d34a3756116772f9074a1b837201
SMARCOCCIA <seb@gmail.fr>
2023-12-21 11:08:09 +0100

    Import : BackButton, NotFound.css. Ajout d'un bouton de retour et amélioration du visuel

--- EventHub_Front/src/_utils/NotFound.jsx
+++ EventHub_Front/src/_utils/NotFound.jsx
@@ -1,17 +1,20 @@
+import { BackButton } from '@components/public/BackButton'
 import React from 'react'
+
+import '../_styles/NotFound.css'
 
 export const NotFound = () => {
   return (
 
- <div className='mt-32 container mx-auto'>
-    <h1 className="text-9xl font-bold text-center my-7">
-        404
-        <span className="text-pink-500">!</span>
-    </h1>
-    <p className="text-center text-xl">Nous n'arrivons pas à trouver cette page...</p>
-    
-    </div>
-
-    
+    <main className="my-10 container mx-auto">
+        <div className="ml-7 my-10">
+            <BackButton path={"/"}/>
+        </div>
+        <h1 id="h1_NotFound" className="text-9xl font-bold text-center my-7">
+            Erreur 404
+            <span className="text-pink-500">!</span>
+        </h1>
+        <p className="text-center text-xl">Nous n'arrivons pas à trouver cette page...</p>
+    </main>
   )
 }
